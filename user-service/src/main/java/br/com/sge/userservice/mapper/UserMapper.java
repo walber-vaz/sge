924ca92de2712641e75eda43901c40f3c72ebc39
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-04-12 17:37:21 -0300

    feat(mapper): add methods for mapping UserCreateRequest and UserResponsePost

--- user-service/src/main/java/br/com/sge/userservice/mapper/UserMapper.java
+++ user-service/src/main/java/br/com/sge/userservice/mapper/UserMapper.java
@@ -1,12 +1,20 @@
 package br.com.sge.userservice.mapper;
 
 import br.com.sge.userservice.domain.User;
+import br.com.sge.userservice.dto.request.UserCreateRequest;
 import br.com.sge.userservice.dto.response.UserGetResponse;
+import br.com.sge.userservice.dto.response.UserResponsePost;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
 
 @Mapper(componentModel = ComponentModel.SPRING)
 public interface UserMapper {
+
+  User toUser(UserCreateRequest userCreateRequest);
+
+  UserResponsePost toUserResponsePost(User user);
+
   List<UserGetResponse> toUserGetResponseList(List<User> users);
 }
