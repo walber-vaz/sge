a413ce4ec5e18ecfcfd966a1373104997363dcda
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-04-12 17:37:51 -0300

    feat(controller): add create method for user registration

--- user-service/src/main/java/br/com/sge/userservice/controller/UserController.java
+++ user-service/src/main/java/br/com/sge/userservice/controller/UserController.java
@@ -1,13 +1,19 @@
 package br.com.sge.userservice.controller;
 
+import br.com.sge.userservice.dto.request.UserCreateRequest;
 import br.com.sge.userservice.dto.response.UserGetResponse;
+import br.com.sge.userservice.dto.response.UserResponsePost;
 import br.com.sge.userservice.mapper.UserMapper;
 import br.com.sge.userservice.service.UserService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
+
   private final UserService service;
   private final UserMapper mapper;
 
@@ -28,5 +35,18 @@ public class UserController {
     var response = mapper.toUserGetResponseList(users);
 
     return ResponseEntity.ok(response);
+  }
+
+  @PostMapping
+  public ResponseEntity<UserResponsePost> create(@RequestBody @Valid UserCreateRequest request) {
+    log.info("Request to create user: {}", request);
+
+    var user = mapper.toUser(request);
+
+    var userSaved = service.store(user);
+
+    var response = mapper.toUserResponsePost(userSaved);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
   }
 }
