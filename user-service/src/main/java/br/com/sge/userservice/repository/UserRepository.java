67b870d87def264e3a272df2cec223fba5335b15
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-04-12 12:28:22 -0300

    feat(repository): add UserRepository interface for user data access

---
+++ user-service/src/main/java/br/com/sge/userservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.sge.userservice.repository;
+
+import br.com.sge.userservice.domain.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
