4419da3580fa37b4249154f22246cbf4cb35bf18
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-04-12 18:44:29 -0300

    feat(service): add user update method and validation checks

--- user-service/src/main/java/br/com/sge/userservice/service/UserService.java
+++ user-service/src/main/java/br/com/sge/userservice/service/UserService.java
@@ -1,9 +1,11 @@
 package br.com.sge.userservice.service;
 
 import br.com.sge.exception.ConflictException;
+import br.com.sge.exception.NotFoundException;
 import br.com.sge.userservice.domain.User;
 import br.com.sge.userservice.repository.UserRepository;
 import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -26,6 +28,22 @@ public class UserService {
   public User store(User user) {
     assertEmailOrPhoneExists(user.getEmail(), user.getPhone());
     return repository.save(user);
+  }
+
+  @Transactional
+  public void update(User user) {
+    assertUserExists(user.getId());
+    assertUserEmailOrPhoneAndId(user.getId(), user.getEmail(), user.getPhone());
+    repository.save(user);
+  }
+
+  public void assertUserExists(UUID id) {
+    repository.findById(id).orElseThrow(() -> new NotFoundException("User not found"));
+  }
+
+  public void assertUserEmailOrPhoneAndId(UUID id, String email, String phone) {
+    repository.findByEmailOrPhoneAndId(email, phone, id)
+        .orElseThrow(() -> new NotFoundException("User not found"));
   }
 
   public void assertEmailOrPhoneExists(String email, String phone) {
