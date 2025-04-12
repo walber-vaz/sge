7746f10f6dad5cfc134e10480d84e0747da952f2
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-04-12 17:37:27 -0300

    feat(repository): add method to find user by email or phone

--- user-service/src/main/java/br/com/sge/userservice/repository/UserRepository.java
+++ user-service/src/main/java/br/com/sge/userservice/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package br.com.sge.userservice.repository;
 
 import br.com.sge.userservice.domain.User;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+  Optional<User> findByEmailOrPhone(String email, String phone);
 }
