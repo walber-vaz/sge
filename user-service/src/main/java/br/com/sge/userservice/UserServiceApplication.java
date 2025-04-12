d83c5c0415e54fb972ecf26ff53aa76f90350dab
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-04-12 12:17:55 -0300

    refactor: rename UserApplication and UserApplicationTests to UserServiceApplication and UserServiceApplicationTests

--- user-service/src/main/java/br/com/sge/userservice/UserServiceApplication.java
+++ user-service/src/main/java/br/com/sge/userservice/UserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserApplication {
+public class UserServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(UserApplication.class, args);
+    SpringApplication.run(UserServiceApplication.class, args);
   }
 
 }
